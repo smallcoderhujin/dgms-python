c8490530f29681d938ddecbd83cbf6a0f0e6ee04
hujin <764236991@qq.com>
2013-05-10 11:08:15 +0800

    none

--- water_quality/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from water_quality.views import *
-
-urlpatterns = patterns('',
-    url(r'^list/$',water_quality_list),
-    url(r'^export/$',export),
-)
